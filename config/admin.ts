b909b9495bcc778388064ea14c40516d94efa6c7
tchappel <thomas.chappel4@gmail.com>
2026-01-02 16:44:51 +0100

    add previews

--- config/admin.ts
+++ config/admin.ts
@@ -1,20 +1,55 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env("ENCRYPTION_KEY"),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+  },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env("CLIENT_URL"),
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId });
+
+        const pathname = getPreviewPathname(uid, { locale, document });
+
+        if (!pathname) {
+          return null;
+        }
+
+        const urlSearchParams = new URLSearchParams({
+          url: pathname,
+          secret: env("PREVIEW_SECRET"),
+          status,
+        });
+
+        return `${env("CLIENT_URL")}/api/preview?${urlSearchParams}`;
+      },
+    },
   },
 });
+
+const getPreviewPathname = (
+  uid: string,
+  { locale, document }: { locale?: string; document: any }
+): string | null => {
+  switch (uid) {
+    case "api::home-page.home-page":
+      return "/";
+    default:
+      return null;
+  }
+};
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -9,8 +9,41 @@ export interface BlocksHero extends Struct.ComponentSchema {
   attributes: {
     backgroundImage: Schema.Attribute.Media<'images'> &
       Schema.Attribute.Required;
+    cta: Schema.Attribute.Component<'shared.cta', false>;
     subtitle: Schema.Attribute.Text & Schema.Attribute.Required;
     title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SharedCta extends Struct.ComponentSchema {
+  collectionName: 'components_shared_ctas';
+  info: {
+    displayName: 'Cta';
+    icon: 'bell';
+  };
+  attributes: {
+    link: Schema.Attribute.Component<'shared.link', false> &
+      Schema.Attribute.Required;
+    variant: Schema.Attribute.Enumeration<['primary', 'accent']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'primary'>;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+    icon: 'link';
+  };
+  attributes: {
+    externalUrl: Schema.Attribute.String;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    page: Schema.Attribute.Relation<'oneToOne', 'api::page.page'>;
+    target: Schema.Attribute.Enumeration<['_self', '_blank']> &
+      Schema.Attribute.DefaultTo<'_self'>;
+    type: Schema.Attribute.Enumeration<['internal', 'external']> &
+      Schema.Attribute.Required;
   };
 }
 
@@ -18,6 +51,8 @@ declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'blocks.hero': BlocksHero;
+      'shared.cta': SharedCta;
+      'shared.link': SharedLink;
     }
   }
 }
