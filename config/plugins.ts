1a205801cbbc29bca7dac87a4abee413a1763bfc
tchappel <thomas.chappel4@gmail.com>
2026-04-12 10:18:51 +0200

    add sendgrid email provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_EMAIL"),
+        defaultReplyTo: env("SENDGRID_EMAIL"),
+      },
+    },
+  },
+});
